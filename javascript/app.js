dd93e926f02abe9e1032143866cd5b76ad2fcf7f
ImashNipun <it21034404@my.sliit.lk>
2023-01-10 23:28:14 +0530

    Define addAnItem function
    The addAnItem function generates a list item for each input when a user enters an item in the input box and clicks the submit button.
    The list item is wrapped around a div element.
    Also, there are two buttons for each list item to delete and mark as done.

---
+++ javascript/app.js
@@ -0,0 +1,37 @@
+var todo_form = document.querySelector('.frm');
+var todo_input = document.querySelector('#item-input');
+var submit_btn = document.querySelector('.submit-btn');
+
+addEventListener('submit',addAnItem);
+
+function addAnItem(event){
+    event.preventDefault();
+
+    if(todo_input.value == ''){
+        alert('Input feild is empty!');
+    }else{
+        var list_container = document.createElement('div');
+        var list_item = document.createElement('li');
+        var delete_btn = document.createElement('button');
+        var done_btn = document.createElement('button');
+
+        list_container.className = 'single-item';
+        delete_btn.className = 'delete-btn';
+        done_btn.className = 'done-btn';
+
+        list_item.innerText = todo_input.value;
+        delete_btn.innerText = 'X';
+        done_btn.innerText = 'Done';
+
+        list_container.appendChild(list_item);
+        list_container.appendChild(delete_btn);
+        list_container.appendChild(done_btn);
+
+        var ordered_list = document.querySelector('ol');
+
+        ordered_list.appendChild(list_container);
+
+        todo_input.value = '';
+
+    }
+}
